93efcf335ce005618e90a12967f080436409d2e3
Frederic Bec <bec.frederic90@outlook.fr>
2025-01-23 08:52:36 +0100

    race condition with lock

--- thread_examples.py
+++ thread_examples.py
@@ -9,13 +9,19 @@ import time
 class FakeDatabase:
     def __init__(self):
         self.value = 0
+        self._lock = threading.Lock()
 
     def update(self, name):
         logging.info("Thread %s: starting update", name)
-        local_copy = self.value
-        local_copy += 1
-        time.sleep(0.1)
-        self.value = local_copy
+        logging.debug("Thread %s about to lock", name)
+        with self._lock:
+            logging.debug("Thread %s has lock", name)
+            local_copy = self.value
+            local_copy += 1
+            time.sleep(0.1)
+            self.value = local_copy
+            logging.debug("Thread %s about to release lock", name)
+        logging.debug("Thread %s after release", name)
         logging.info("Thread %s: finishing update", name)
 
 
@@ -36,17 +42,4 @@ if __name__ == "__main__":
         for index in range(2):
             executor.submit(database.update, index)
     logging.info("Testing update. Ending value is %d.", database.value)
-    # threads = list()
-    # for index in range(3):
-    #     logging.info("Main    : create and start thread %d.", index)
-    #     x = threading.Thread(target=thread_function, args=(index,))
-    #     threads.append(x)
-    #     x.start()
-    # for index, thread in enumerate(threads):
-    #     logging.info("Main    : before joining thread %d.", index)
-    #     thread.join()
-    #     logging.info("Main    : thread %d done", index)
-    # print(len(threads))
 
-    # with concurrent.futures.ThreadPoolExecutor(max_workers=3) as executor:
-    #     executor.map(thread_function, range(3))
