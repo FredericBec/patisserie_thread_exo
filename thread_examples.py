29b64ed8f99ca9edf8a49dfde90af9f02b93c4e4
Frederic Bec <bec.frederic90@outlook.fr>
2025-01-23 08:33:33 +0100

    thread pool executor

--- thread_examples.py
+++ thread_examples.py
@@ -1,3 +1,4 @@
+import concurrent.futures
 import logging
 
 import threading
@@ -15,14 +16,17 @@ if __name__ == "__main__":
     format = "%(asctime)s: %(message)s"
     logging.basicConfig(format=format, level=logging.INFO,
                         datefmt="%H:%M:%S")
-    threads = list()
-    for index in range(3):
-        logging.info("Main    : create and start thread %d.", index)
-        x = threading.Thread(target=thread_function, args=(1,))
-        threads.append(x)
-        x.start()
-    for index, thread in enumerate(threads):
-        logging.info("Main    : before joining thread %d.", index)
-        thread.join()
-        logging.info("Main    : thread %d done", index)
+    # threads = list()
+    # for index in range(3):
+    #     logging.info("Main    : create and start thread %d.", index)
+    #     x = threading.Thread(target=thread_function, args=(index,))
+    #     threads.append(x)
+    #     x.start()
+    # for index, thread in enumerate(threads):
+    #     logging.info("Main    : before joining thread %d.", index)
+    #     thread.join()
+    #     logging.info("Main    : thread %d done", index)
     # print(len(threads))
+
+    with concurrent.futures.ThreadPoolExecutor(max_workers=3) as executor:
+        executor.map(thread_function, range(3))
