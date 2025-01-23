a0a034c19cd91e24ea53390a17e54e549e4da823
Frederic Bec <bec.frederic90@outlook.fr>
2025-01-23 15:33:21 +0100

    Modify thread class

--- Thread/meltingChocolate.py
+++ Thread/meltingChocolate.py
@@ -1,13 +1,13 @@
-import math
 import time
 
+from Models.recipe import Recipe
 from Thread.helper import Helper
-from Models.chocolate import Chocolate
 
 
 class MeltingChocolate(Helper):
-    def __int__(self, chocolate: Chocolate):
-        self.quantity = chocolate.quantity
+    def __init__(self, recipe: Recipe):
+        super().__init__()
+        self.recipe_helper = recipe
 
     def run(self):
         print("Je mets de l'eau à chauffer dans une bouilloire")
@@ -17,7 +17,4 @@ class MeltingChocolate(Helper):
         print("J'y pose le bol rempli de chocolat")
         time.sleep(1)
 
-        nb_turns = math.ceil(self.quantity / 10)
-        for nb_turn in range(1, nb_turns + 1):
-            print(f"Je mélange {self.quantity} de chocolat à fondre, tour n°{nb_turn}")
-            time.sleep(1)
+        self.recipe_helper.melt()
--- Thread/spout.py
+++ Thread/spout.py
@@ -1,6 +1,22 @@
+import threading
+import time
+
+from Models.recipe import Recipe
 from Thread.helper import Helper
 
 
 class Spout(Helper):
+    def __init__(self, recipe: Recipe):
+        super().__init__()
+        self.recipe_helper = recipe
+        self.value = 0
+        self.lock = threading.Lock()
 
     def run(self):
+        with self.lock:
+            print(f"Je verse du {self.recipe_helper.ingredient}")
+            local_copy = self.value
+            local_copy += 1
+            time.sleep(0.5)
+            self.value = local_copy
+            print(f"{self.value}")
