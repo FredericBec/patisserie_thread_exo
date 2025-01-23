4ad49cf4307e1a770f2721f627146794a6bd19cf
Frederic Bec <bec.frederic90@outlook.fr>
2025-01-23 08:40:57 +0100

    race condition with fake database

--- thread_examples.py
+++ thread_examples.py
@@ -6,6 +6,19 @@ import threading
 import time
 
 
+class FakeDatabase:
+    def __init__(self):
+        self.value = 0
+
+    def update(self, name):
+        logging.info("Thread %s: starting update", name)
+        local_copy = self.value
+        local_copy += 1
+        time.sleep(0.1)
+        self.value = local_copy
+        logging.info("Thread %s: finishing update", name)
+
+
 def thread_function(name):
     logging.info("Thread %s: starting", name)
     time.sleep(2)
@@ -16,6 +29,13 @@ if __name__ == "__main__":
     format = "%(asctime)s: %(message)s"
     logging.basicConfig(format=format, level=logging.INFO,
                         datefmt="%H:%M:%S")
+
+    database = FakeDatabase()
+    logging.info("Testing update. Starting value is %d.", database.value)
+    with concurrent.futures.ThreadPoolExecutor(max_workers=2) as executor:
+        for index in range(2):
+            executor.submit(database.update, index)
+    logging.info("Testing update. Ending value is %d.", database.value)
     # threads = list()
     # for index in range(3):
     #     logging.info("Main    : create and start thread %d.", index)
@@ -28,5 +48,5 @@ if __name__ == "__main__":
     #     logging.info("Main    : thread %d done", index)
     # print(len(threads))
 
-    with concurrent.futures.ThreadPoolExecutor(max_workers=3) as executor:
-        executor.map(thread_function, range(3))
+    # with concurrent.futures.ThreadPoolExecutor(max_workers=3) as executor:
+    #     executor.map(thread_function, range(3))
